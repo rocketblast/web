d094e3f7e131795d054328fb4afecba794dbb2ef
pompa <pompa@faceit.com>
2018-04-05 19:18:47 +0100

    fixed consistent logo size

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="Center">
-        <img src={logo} alt="logo" />
+        <img src={logo} className="Logo" alt="logo" />
       </div>
     );
   }
