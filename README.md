bc84729c70e1aec9d9bd2d20b2d3f0d6dda60a37
pompa <pompa@faceit.com>
2018-04-05 17:34:14 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+The beginning of something beautiful.
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="Center">
+        <img src={logo} alt="logo" />
+      </div>
+    );
+  }
+}
+
+export default App;
